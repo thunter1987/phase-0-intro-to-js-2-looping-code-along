d278af3687d4c3ca83ce389aef481a09387139e9
Tony Hunter <thunter1987@yahoo.com>
2023-03-10 15:01:49 -0600

    "Loop Tests Complete"

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solutions in this file
+function writeCards(name, type) {
+    let messages = [];
+    for (let i = 0; i < name.length; i++) [
+            messages.push("Thank you, " + name[i] + ", for the wonderful " + type + " gift!")
+    ]
+    return messages;
+}
+function countDown() {
+    let count = 10;
+    while (count >= 0) [
+        console.log (count--)
+    ]
+}
